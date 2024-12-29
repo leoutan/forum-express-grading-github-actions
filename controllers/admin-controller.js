402841c338ce9c46c841e795b08256c38be61b83
Leo Tseng <leo81xi16@gmail.com>
2024-12-29 17:00:45 +0800

    modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -5,10 +5,17 @@
 // }
 
 // module.exports = adminController
+// const { raw } = require('express')
+const { Restaurant } = require('../models')
+// const restaurant = require('../models/restaurant')
 
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({ raw: true })
+      .then(restaurants => {
+        res.render('admin/restaurants', { restaurants })
+      })
+      .catch(error => next(error))
   }
 }
 
